d06beb1ca3155ec1fa8bc1d3f311353645252dfe
Daniel Bell <spaniel2620@googlemail.com>
2012-11-14 09:42:18 +0000

    renamed some stuff

---
+++ code/Timetable/WindowsApplication3/Group.vb
@@ -0,0 +1,13 @@
+﻿Public Class Group
+    Private m_group As String
+
+    Public Sub New(ByVal cName As String)
+        m_group = cName
+    End Sub
+
+    ReadOnly Property gName() As String
+        Get
+            Return m_group
+        End Get
+    End Property
+End Class
