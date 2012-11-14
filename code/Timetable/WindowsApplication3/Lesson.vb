e681a2fe09cc6ecd2502ff92a2c5de62ce3d776e
StevenCooke14 <stevencooke14@gmail.com>
2012-11-14 09:41:35 +0000

    Finished module class code to add a lesson

--- code/Timetable/WindowsApplication3/Lesson.vb
+++ code/Timetable/WindowsApplication3/Lesson.vb
@@ -1,22 +1,25 @@
 ﻿Public Class Lesson
 
     Private m_room As Room
+    Private m_module As Modules
 
-    Public Sub New(ByVal roomName As String)
+    Public Sub New(ByVal roomName As String, moduleCode As String)
 
         m_room = New Room(roomName)
+        m_module = New Modules(moduleCode)
 
     End Sub
 
     Public Function displayString() As String
 
         Return m_room.name()
+        Return m_module.moduleCode()
 
     End Function
 
     Public Sub saveData(ByRef writer As IO.StreamWriter)
         Dim mday As String
         mday = "Monday"
-        writer.WriteLine(m_room.name() + "," + mday)
+        writer.WriteLine(m_room.name() + "," + m_module.moduleCode() + "," + mday)
     End Sub
 End Class
